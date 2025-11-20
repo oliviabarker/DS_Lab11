56f41f9208fd6b3c71cd22fb7b49e96645d3f638
Olivia Barker <olivia.barker@centre.edu>
2025-11-20 14:07:30 -0500

    Lab 11 Tests

--- src/test/java/TestTree.java
+++ src/test/java/TestTree.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
@@ -11,7 +12,7 @@ public class TestTree
 	
 	Scanner comp;
 	PrintStream me;
-	boolean show = true; //if set to true, will print out respones to and from the game.  Set to false to make it faster.
+	boolean show = true; //if set to true, will print out responses to and from the game.  Set to false to make it faster.
 	
     
     //initializes the Testing environment with piped streams (Queues)
@@ -68,11 +69,67 @@ public class TestTree
 		check(Strings.IS_IT_ALIVE);
         say("Y");
         //now what? Think of all the input and outputs here...
-		
-
-        
-        
-        //close the streams at the end to enrue good behavior.
+		check(Strings.IS_IT_A+Strings.DUCK+"?");
+		say("N");
+		check(Strings.WHAT_IS_THE_ANSWER);
+		say("Cat");
+		check(Strings.NEW_QUESTION+Strings.DUCK+" and a Cat");
+		say("Is it a pet?");
+		check("Answering yes to Is it a pet? means Cat?");
+		say("Y");
+		check(Strings.THANKS);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+        say("Y");
+        check("Is it a pet?");
+        say("Y");
+        check("Is it a Cat?");
+        say("Y");
+        check(Strings.I_WIN);
+        check(Strings.PLAY_AGAIN);
+        say("Y");
+		check(Strings.IS_IT_ALIVE);
+        say("N");
+		check(Strings.IS_IT_A+Strings.ROCK+"?");
+		say("N");
+		check(Strings.WHAT_IS_THE_ANSWER);
+		say("Pencil");
+		check(Strings.NEW_QUESTION+Strings.ROCK+" and a Pencil");
+		say("Is it an office supply?");
+		check("Answering yes to Is it an office supply? means Pencil?");
+		say("Y");
+		check(Strings.THANKS);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+        say("N");
+		check("Is it an office supply?");
+		say("N");
+		check(Strings.IS_IT_A+Strings.ROCK+"?");
+		say("N");
+		check(Strings.WHAT_IS_THE_ANSWER);
+		say("Phone");
+		check(Strings.NEW_QUESTION+Strings.ROCK+" and a Phone");
+		say("Is it part of nature?");
+		check("Answering yes to Is it part of nature? means Phone?");
+		say("N");
+		check(Strings.THANKS);
+		check(Strings.PLAY_AGAIN);
+		say("Y");
+		check(Strings.IS_IT_ALIVE);
+        say("N");
+		check("Is it an office supply?");
+		say("N");
+		check("Is it part of nature?");
+		say("N");
+		check(Strings.IS_IT_A+"Phone"+"?");
+		say("Y");
+		check(Strings.I_WIN);
+		check(Strings.PLAY_AGAIN);
+		say("N");
+	//	assertThrows(IOException.class, ()->{say("k")});
+        //close the streams at the end to ensure good behavior.
 		comp.close();
 		me.close();
 	}
